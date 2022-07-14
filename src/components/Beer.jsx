dc41f1b81c14b1eac1818708c699828c7314a698
Andrew <marianoandres191200@gmail.com>
2022-07-14 20:57:39 -0300

    pagination started

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -1,13 +1,24 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles/beer.css';
 
-const Beer = ({beer}) =>{
-    return(
+const Beer = ({ beer }) => {
+    const { image_url, name, srm } = beer
+    
+    return (
         <div className="our-beers-beer">
             <div className="our-beers-beer-img">
-                <img src={beer.image_url} alt="beer img"/>
+                <img src={image_url} alt="beer img" />
             </div>
             <div className="our-beers-beer-info">
-
+                <div className="our-beers-beer-name">
+                    <span>{name}</span>
+                </div>
+                <div className="our-beers-price-rate">
+                    <p>4.9 <FontAwesomeIcon icon={faStar} /></p>
+                    <p className="our-beers-price">{srm}</p>
+                </div>
             </div>
         </div>
     )
--- src/components/OurBeers.jsx
+++ src/components/OurBeers.jsx
@@ -1,16 +1,34 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import '../styles/ourbeers.css';
 import GetBeers from '../utils/GetBeers';
 import Beer from './Beer';
 
-const OurBeers = () =>{
-    const [beers,setBeers] = useState([])
-    GetBeers().then(beers => setBeers(beers))
+const OurBeers = () => {
+    const [beers, setBeers] = useState([]);
+    const [page, setPage] = useState(1);
+    const [maxPerPage, setMaxPerPage] = useState(15);
+    const [lastPage, setLastPage] = useState(0);
 
-    return(
+    console.log(((page - 1) * maxPerPage))
+    console.log((page*maxPerPage))
+
+    useEffect(() => {
+        GetBeers().then(beers => setBeers(beers))
+        setLastPage(Math.ceil(beers.length/maxPerPage))
+    }, [maxPerPage, beers.length])
+
+    return (
         <div className="our-beers-main-container">
             <div className="our-beers-container">
-                {beers.map(beer => <Beer beer={beer}/>)}
+                {
+                    beers.slice(((page - 1) * maxPerPage), (page*maxPerPage)).map(beer => <Beer beer={beer} />)
+                }
+            </div>
+            <div className="pagination">
+                    <p>Page {page} of {lastPage}</p>
+                    <button>prev</button>
+                    <button>next</button>
             </div>
         </div>
     )
